14adf37308f69af9c4d7bfcf2bf43df475c70896
Viral-Ahir <ahirviral01@gmail.com>
2024-12-04 23:17:20 -0500

    Added other comment

--- server.ts
+++ server.ts
@@ -29,7 +29,7 @@ db.on("connected", () => {
 // cors policy that allows requests from all origins.
 // This is insecure and should be changed in production
 // to set trusted hosts
-// add comment
+// add comment 1
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
